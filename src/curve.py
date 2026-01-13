38d79fe533c59731a6fd4e0c6bd04d972277eae7
Angus Clark <77604209+T3nnysonz@users.noreply.github.com>
2026-01-13 14:28:31 +0200

    slight extension
    added lost datapoints

--- src/curve.py
+++ src/curve.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def interpolate(datapoints, grain = 10):
+    # datapoints: array of tuples, each tuple should be of form (float, float) and containts
+    # in order the time of the datapoint and the difference factor at that time
     # grain: Integer, how fine the interpolation should be
     
     # These lines ensure grain is an integer greater than 1
@@ -10,21 +12,22 @@ def interpolate(datapoints, grain = 10):
         grain = 1
     
     
-    # Generates 10 interpolated datapoints between inputted datapoints
-    pillars_t, pillars_df = zip(*datapoints)
+    # Generates interpolated datapoints between inputted datapoints
+    pillars_t, pillars_df = zip(*datapoints) # Seperating tuples
     new_pillars_t = []
-    log_df = np.log(pillars_df)
+    log_df = np.log(pillars_df) # preparing for linearly interpolating between log of datapoints
     new_pillars_df = []
-    for t in range(len(pillars_t)-1):
-        interval_length = pillars_t[t+1]-pillars_t[t]
-        df_difference = log_df[t+1]-log_df[t]
-        for i in range(grain):
+    for t in range(len(pillars_t)-1): # For every time pillar
+        interval_length = pillars_t[t+1]-pillars_t[t] # Find distance to next time pillar
+        df_difference = log_df[t+1]-log_df[t] # Calculate difference in DF values at the time pillars
+        for i in range(grain): # linearly adds datapoints between the pillars
             new_pillars_t.append(pillars_t[t]+(i/grain)*interval_length)
             new_pillars_df.append(log_df[t]+(i/grain)*df_difference)
+    new_pillars_t.append(pillars_t[-1])
+    new_pillars_df.append(np.log(pillars_df[-1]))
     
-    new_pillars_df = np.exp(new_pillars_df)
-    new_data = zip(new_pillars_t, new_pillars_df)
-    #print(new_data)
+    new_pillars_df = np.exp(new_pillars_df) #reverts the logarithmic data into standard data
+    new_data = zip(new_pillars_t, new_pillars_df) # recombines the time and DFs
     return new_data
 
 def plot(datapoints):
@@ -33,8 +36,9 @@ def plot(datapoints):
     plt.xlabel('Time (years)')
     plt.ylabel('Discount Factor')
     plt.title('Discount Factor Curve')
+    plt.grid()
     plt.show()
     return 0
 
-data = [(0,1), (1,0.5)]
+data = [(0,1), (1,0.9), (2,0.75),(3,0.4),(4,0.1)]
 plot(interpolate(data))
