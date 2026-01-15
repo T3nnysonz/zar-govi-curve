7bf49a7a6854789f78f73916c73bb4ecdeaa83a2
Angus Clark <77604209+T3nnysonz@users.noreply.github.com>
2026-01-15 11:23:52 +0200

    untested but complete bootstrapping function

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -6,6 +6,7 @@ from src.curve import DiscountCurve
 
 def bootstrap_govi_curve(bonds, settlement_date, conventions = ""):
     sorted_bonds = sorted(bonds, key= lambda b: b['mature_date'])
+    DF_data = []
     
     for bond in sorted_bonds:
         known = 0
@@ -17,7 +18,8 @@ def bootstrap_govi_curve(bonds, settlement_date, conventions = ""):
         i = len(cashflows)
         last_coupon = (pd.Timestamp(sorted_bonds[0]["mature_date"]) - pd.DateOffset(months=6)).date()
         next_coupon = sorted_bonds[0]["mature_date"]
-        known_dfs = [(0,1)]
+        known_dfs = []
+        unknown_cashflows = []
         
         dirtyPrice = dirty_price(clean_price, settlement_date, last_coupon, next_coupon, coupon_rate, face_value)
         
@@ -27,7 +29,17 @@ def bootstrap_govi_curve(bonds, settlement_date, conventions = ""):
                 year_frac = year_fraction(settlement_date, date)
                 df = DiscountCurve([known_dfs])
                 known += flow * df.calcDF(date)
-        
-        new_DF = (dirtyPrice-known)/1
-    DF_data = 0
+                i+=1
+            else:
+                unknown_cashflows.append((date, flow))
+        if(len(unknown_cashflows)==2):
+            date1, amount1 = unknown_cashflows[0]
+            filler, amount2 = unknown_cashflows[1]
+            final_payment = amount1+amount2
+            new_DF = (dirtyPrice-known)/final_payment
+            DF_data.append(date1, new_DF)
+            known_dfs.append((year_frac, new_DF))
+        else:
+            print("Unable to calculate df's")
+            return [];
     return DF_data;
